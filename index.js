12381d894b9981162db5b6b6429b2ca53c539901
Ronson Wuniardy <rwuniard@hotmail.com>
2019-03-10 23:04:22 -0400

    Adding Rest API

--- index.js
+++ index.js
@@ -1,10 +1,46 @@
 const express = require('express');
-
+const path = require('path');
 const app = express();
+const members = require('./Members');
+const logger = require('./middleware/logger')
 
-app.get('/', (req, res) => {
-    res.send('Hello World!!!');
+
+
+
+// Init middleware logger.
+// app.use(logger);
+
+
+// app.get('/', (req, res) => {
+//     // res.send('Hello World!!!');
+   
+//     filename = path.join(__dirname, 'public', 'index.html');
+//     console.log(filename);
+//     res.sendFile(filename);
+// });
+
+
+// Set REST api
+// Get all members.
+app.get('/api/members', (req, res) => {
+    res.json(members);
 });
+
+
+// Get Single Member
+app.get('/api/members/:id', (req, res) => {
+    const found = members.some(member => member.id === parseInt(req.params.id));
+    if (found) {
+        res.json(members.filter(member => member.id === parseInt(req.params.id)));
+    }
+    else {
+        res.status(400).json({msg: `No member with the id of ${req.params.id}`});
+    }
+    
+});
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
 
 const PORT = process.env.PORT || 5000;
 
