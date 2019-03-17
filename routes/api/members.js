dcf63a9bacca99c48a4010fb80266afc94985a56
Ronson Wuniardy <rwuniard@hotmail.com>
2019-03-17 13:37:06 -0400

    Add update and delete

--- routes/api/members.js
+++ routes/api/members.js
@@ -43,4 +43,38 @@ router.post('/', (req, res) => {
     }
 });
 
+// Update Member
+router.put('/:id', (req, res) => {
+    const found = members.some(member => member.id === parseInt(req.params.id));
+    if (found) {
+       const updMember = req.body;
+       members.forEach(member => {
+           if (member.id === parseInt(req.params.id)) {
+               member.name = updMember.name ? updMember.name : req.body.name;
+               member.email = updMember.email ? updMember.email : req.body.email;
+               res.json({ msg: 'Member updated', member});
+           }
+       })
+    }
+    else {
+        res.status(400).json({msg: `No member with the id of ${req.params.id}`});
+    }
+    
+});
+
+// Delete Member
+router.delete('/:id', (req, res) => {
+    const found = members.some(member => member.id === parseInt(req.params.id));
+    if (found) {
+       res.json({
+           msg: 'Member deleted',
+           members: members.filter(member => member.id !== parseInt(req.params.id))
+       });
+    }
+    else {
+        res.status(400).json({msg: `No member with the id of ${req.params.id}`});
+    }
+    
+});
+
 module.exports = router;
